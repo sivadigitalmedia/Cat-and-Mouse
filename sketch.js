95e7a888f649048dc9ff9c9f6401ea5204aa90c3
sivadigitalmedia <84766359+sivadigitalmedia@users.noreply.github.com>
2021-06-21 12:21:43 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,32 @@
+var garden,gardenImg;
+var cat,cat1,cat2,cat3,cat4;
+var mouse,mouse1,mouse2,mouse3,mouse4;
+
+function preload() {
+    //load the images here
+    gardenImg = loadImage("images/garden.png");
+    
+}
+
+function setup(){
+    createCanvas(1000,800);
+    //create tom and jerry sprites here
+
+
+}
+
+function draw() {
+
+    background(255);
+    //Write condition here to evalute if tom and jerry collide
+
+    drawSprites();
+}
+
+
+function keyPressed(){
+
+  //For moving and changing animation write code here
+
+
+}
